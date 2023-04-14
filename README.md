688c1be17ac3ee8388113c165feef1094c27edfb
shawee <757221692@qq.com>
2023-04-15 03:35:06 +0800

    更新合约部署脚本

--- README.md
+++ README.md
@@ -58,18 +58,43 @@ npm install rimraf --save-dev
   },
 ```
 
-修改部署脚本 deploy.js,获取部署合约地址
+修改部署脚本 deploy.js
 
 ```js
-let contractInfo = {};
-// 每部署一个合约调用一次,记录合约地址
-function recordDeployedContract(contractName, chainId, address) {
-  contractInfo = {
-    ...contractInfo,
-    [contractName]: {
-      [chainId]: address,
-    },
-  };
+const { ethers } = require("hardhat");
+
+let contractToDeploy = [
+  {
+    name: "Lock",
+    params: [
+      Math.round(Date.now() / 1000) + 60,
+      { value: hre.ethers.utils.parseEther("0.001") },
+    ],
+  },
+];
+
+let contractDeployedInfo = {};
+
+async function main() {
+  [deployer, addr1, addr2] = await ethers.getSigners();
+  console.log(`========== Deploy contract by ${deployer.address} ==========`);
+
+  const chainId = ethers.provider.network.chainId;
+  for (let contract of contractToDeploy) {
+    const contracFactory = await ethers.getContractFactory(contract.name);
+    const contractInstance = await contracFactory.deploy(...contract.params);
+    await contractInstance.deployed();
+    contractDeployedInfo = {
+      ...contractDeployedInfo,
+      [contract.name]: {
+        [chainId]: contractInstance.address,
+      },
+    };
+    console.log(
+      `success deploy contract: ${contract.name} | chainId: ${chainId} | address: ${contractInstance.address}`
+    );
+  }
+  exportDeployedContractInfo();
 }
 
 // 部署合约脚本执行完,导出所有已部署合约地址
@@ -77,17 +102,25 @@ function exportDeployedContractInfo() {
   const fs = require("fs");
   fs.writeFile(
     "./artifacts/contracts/contractInfo.json",
-    JSON.stringify(contractInfo),
+    JSON.stringify(contractDeployedInfo),
     (err) => {
       if (err) {
-        console.log("=========== 导出部署合约信息失败! ==========");
-        console.log(err);
+        console.log(
+          "=========== Faied to export deplyed contract info! ==========",
+          err
+        );
       } else {
-        console.log("=========== 导出部署合约信息成功! ==========");
+        console.log(
+          "=========== Success to export deployed contract info! =========="
+        );
       }
     }
   );
 }
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
 ```
 
 修改 hardhat.config.js 添加 networks
--- bc_server/scripts/deploy.js
+++ bc_server/scripts/deploy.js
@@ -1,44 +1,37 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
-let contractInfo = {};
+let contractToDeploy = [
+  {
+    name: "Lock",
+    params: [
+      Math.round(Date.now() / 1000) + 60,
+      { value: hre.ethers.utils.parseEther("0.001") },
+    ],
+  },
+];
+
+let contractDeployedInfo = {};
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  [deployer, addr1, addr2] = await ethers.getSigners();
+  console.log(`========== Deploy contract by ${deployer.address} ==========`);
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-  let chainId = hre.ethers.provider.network.chainId;
-  let address = lock.address;
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-  recordDeployedContract("Lock", chainId, address);
-
+  const chainId = ethers.provider.network.chainId;
+  for (let contract of contractToDeploy) {
+    const contracFactory = await ethers.getContractFactory(contract.name);
+    const contractInstance = await contracFactory.deploy(...contract.params);
+    await contractInstance.deployed();
+    contractDeployedInfo = {
+      ...contractDeployedInfo,
+      [contract.name]: {
+        [chainId]: contractInstance.address,
+      },
+    };
+    console.log(
+      `success deploy contract: ${contract.name} | chainId: ${chainId} | address: ${contractInstance.address}`
+    );
+  }
   exportDeployedContractInfo();
-}
-
-// 每部署一个合约调用一次,记录合约地址
-function recordDeployedContract(contractName, chainId, address) {
-  contractInfo = {
-    ...contractInfo,
-    [contractName]: {
-      [chainId]: address,
-    },
-  };
 }
 
 // 部署合约脚本执行完,导出所有已部署合约地址
@@ -46,19 +39,21 @@ function exportDeployedContractInfo() {
   const fs = require("fs");
   fs.writeFile(
     "./artifacts/contracts/contractInfo.json",
-    JSON.stringify(contractInfo),
+    JSON.stringify(contractDeployedInfo),
     (err) => {
       if (err) {
-        console.log("=========== 导出部署合约信息失败! ==========");
-        console.log(err);
+        console.log(
+          "=========== Faied to export deplyed contract info! ==========",
+          err
+        );
       } else {
-        console.log("=========== 导出部署合约信息成功! ==========");
+        console.log(
+          "=========== Success to export deployed contract info! =========="
+        );
       }
     }
   );
 }
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
